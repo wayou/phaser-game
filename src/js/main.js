c58bacb14434f50e7eda612ed139b5c2c9c5a4d6
wayou <liuwayong@gmail.com>
2014-12-28 16:37:28 +0800

    flappy bird clone

--- src/js/main.js
+++ src/js/main.js
@@ -1,143 +1,81 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+'use strict'
 
-function preload() {
+var game = new Phaser.Game(400, 490, Phaser.AUTO, 'gameDiv');
 
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+var mainState = {
+    preload: function() {
+        game.stage.backgroundColor = '#71c5cf';
 
-}
+        game.load.image('bird', 'assets/bird.png');
+        game.load.image('pipe', 'assets/pipe.png');
+    },
+    create: function() {
+        //enable physics
+        game.physics.startSystem(Phaser.Physics.ARCADE);
 
-var player;
-var platforms;
-var cursors;
+        this.bird = game.add.sprite(100, 245, 'bird');
 
-var stars;
-var score = 0;
-var scoreText;
+        //enable gravity on the bird
+        game.physics.arcade.enable(this.bird);
+        this.bird.body.gravity.y = 1000;
 
-function create() {
+        this.pipes = game.add.group();
+        this.pipes.enableBody = true; //add physics to the group
+        this.pipes.createMultiple(20, 'pipe'); //create 20 pipes
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+        this.score = -1;
+        this.labelScore = game.add.text(20, 20, '0', {
+            font: '30px arial',
+            fill: '#ffffff'
+        });
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
+        var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+        spaceKey.onDown.add(this.jump, this);
+    },
+    update: function() {
+        if (!this.bird.inWorld) {
+            this.restartGame();
+        }
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
 
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
+    },
+    jump: function() {
+        this.bird.body.velocity.y = -350;
+    },
+    restartGame: function() {
+        game.state.start('main');
+    },
+    addOnePipe: function(x, y) {
+        //get the first dead pipe of out pipes group
+        var pipe = this.pipes.getFirstDead();
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+        //set the new position of the pipe
+        pipe.reset(x, y);
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
+        //add velocity to the pipe to make it move left
+        pipe.body.velocity.x = -200;
 
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
+        //kill the pipe when its no longer visible
+        pipe.checkWorldBounds = true;
+        pipe.outOfBoundsKill = true;
+    },
+    addRowOfPipes: function() {
+        //decide where the hole will be
+        var hole = Math.floor(Math.random() * 5) + 1;
 
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-    ledge.body.immovable = true;
+        //add the 6 pipes
+        for (var i = 0; i < 8; i++) {
+            if (i !== hole && i !== hole + 1) {
+                this.addOnePipe(400, i * 60 + 10);
+            }
+        }
 
-    ledge = platforms.create(-150, 250, 'ground');
-    ledge.body.immovable = true;
-
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
-
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
-
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
-
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-
-    //  Finally some stars to collect
-    stars = game.add.group();
-
-    //  We will enable physics for any star that is created in this group
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++)
-    {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 300;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        this.score += 1;
+        this.labelScore.text = this.score;
     }
+};
 
-    //  The score
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-
-    //  Our controls.
-    cursors = game.input.keyboard.createCursorKeys();
-    
-}
-
-function update() {
-
-    //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
-    game.physics.arcade.collide(stars, platforms);
-
-    //  Checks to see if the player overlaps with any of the stars, if he does call the collectStar function
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
-
-    //  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
-
-    if (cursors.left.isDown)
-    {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
-    }
-    else if (cursors.right.isDown)
-    {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-    }
-    else
-    {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
-    }
-    
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down)
-    {
-        player.body.velocity.y = -350;
-    }
-
-}
-
-function collectStar (player, star) {
-    
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
-
-}
+game.state.add('main', mainState);
+game.state.start('main');
